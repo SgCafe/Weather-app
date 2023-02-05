663f81289348e51885a28ac6aa218416008f9bd5
Sérgio Gabriel <sgabriel.cafe@gmail.com>
2023-02-05 15:48:32 -0300

    feat: add variables and api key

---
+++ js/script.js
@@ -0,0 +1,15 @@
+//Variable and elements selection
+const apiKey = "143eb49867f301ac207f8076a21a9935";
+
+const inputTxt = document.querySelector("#inputText");
+const Btnsearch = document.querySelector("#searchBtn");
+
+const tempMinElement = document.querySelector("#min-temp");
+const tempNormalElement = document.querySelector("#normal-temp");
+const tempMaxElement = document.querySelector("#max-temp");
+const descriptionElement = document.querySelector("#description span");
+const imgDescriptionElement = document.querySelector("#description i");
+const humidityElement = document.querySelector("#humidity span");
+const windElement = document.querySelector("#wind span");
+const cityElement = document.querySelector("#city span");
+const dateElement = document.querySelector("#date");
