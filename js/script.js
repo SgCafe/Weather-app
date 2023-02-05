eaff5c8dd471b156c14c10a9e75d64ec3d658127
Sérgio Gabriel <sgabriel.cafe@gmail.com>
2023-02-05 15:49:33 -0300

    feat: add btn search and consult data to api

--- js/script.js
+++ js/script.js
@@ -13,3 +13,27 @@ const humidityElement = document.querySelector("#humidity span");
 const windElement = document.querySelector("#wind span");
 const cityElement = document.querySelector("#city span");
 const dateElement = document.querySelector("#date");
+
+//functions
+
+//Função para pegar os dados da API
+const getWeatherData = async (cityInput) => {
+  const response = await fetch(
+    `https://api.openweathermap.org/data/2.5/weather?q=${cityInput}&units=metric&appid=${apiKey}&lang=pt_br`
+  );
+
+  const data = await response.json();
+  console.log(data);
+  return data;
+};
+
+//Função para mostrar dados da API
+const showWeatherData = async (cityInput) => {};
+
+//Events
+Btnsearch.addEventListener("click", (event) => {
+  event.preventDefault;
+
+  const cityInput = inputTxt.value;
+  showWeatherData(cityInput);
+});
